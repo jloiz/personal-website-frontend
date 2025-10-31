5febe5f22cc8035eb65744153061eeedaba0ab49
jloiz <local.dev@mail.com>
2025-10-31 22:40:35 +0000

    visually render icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,20 @@
+import Linkedin from "../sprites/linkedin";
+import Github from "../sprites/github";
+
+
+
 export default function Footer() {
-  return(
+  return (
     <div className="border-t border-divider">
-    <footer>
-        Test footer text
+      <footer className="flex flex-row ">
+        Additional Footer info here
+        <div className="mt-5 mb-3 ml-auto pr-8">
+          <div className="flex flex-row space-x-4">
+            <Github />
+            <Github />
+          </div>
+        </div>
       </footer>
     </div>
-
-  )
+  );
 }
