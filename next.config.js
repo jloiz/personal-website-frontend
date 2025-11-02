f65b1d39c309260a0f010547a1021bfd37fb86c4
jloiz <local.dev@mail.com>
2025-11-02 19:46:27 +0000

    add base path, take to main

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const repo = process.env.GITHUB_REPO || 'personal-website-frontend'
 const nextConfig = {
   output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`
 };
 
 module.exports = nextConfig;
