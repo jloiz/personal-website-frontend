7b1024562c14e23fe8ed090b634b42486a1cb68a
jloiz <local.dev@mail.com>
2025-11-02 21:05:45 +0000

    add minor style

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,8 +2,13 @@
 
 export default function() {
   return(
-    <p>
-      Profile Page content coming soon!
-    </p>
+    <>
+      <div className="flex flex-row item-center mb-2; justify-center">
+        <h1 className="mt-5 text-3xl font-bold underline">Profile</h1>
+      </div>
+      <p>
+        Profile Page content coming soon!
+      </p>
+    </>
   )
 }
