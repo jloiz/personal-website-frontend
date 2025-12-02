75bace51c2b2ef95d2901dab808a203d9dd0419c
jloiz <local.dev@mail.com>
2025-12-02 20:57:25 +0000

    grid template for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,12 +3,26 @@
 import ComingSoon from "../../components/coming-soon";
 
 export default function Projects() {
+
+  const projects = ["website-frontend", "to-do-go"]
+
   return (
     <>
       <div className="flex flex-row item-center mb-2; justify-center">
-        <h1 className="mt-5 text-3xl font-bold underline">Projects</h1>
+        <h1 className="mt-3 text-3xl font-bold underline">Projects</h1>
       </div>
-      <ComingSoon pageTitle={"Projects"} />
+      <div className="flex flex-row items-center justify-center px-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 w-full px-2 sm:px-16">
+          {projects.map((proj) => {
+          return(
+            <p>
+              {proj}
+            </p>
+          )
+        })}
+        </div>
+
+      </div>
     </>
   );
 }
